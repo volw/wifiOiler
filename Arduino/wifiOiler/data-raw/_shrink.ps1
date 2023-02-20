4ff2469a722e604c1b167a78cc881b4fa1f80c30
volw <volw@gmx.de>
2023-02-20 10:53:17 +0100

    shrink script

--- Arduino/wifiOiler/data-raw/_shrink.ps1
+++ Arduino/wifiOiler/data-raw/_shrink.ps1
@@ -1,18 +1,25 @@
 $source = "."
 $target = "..\data"
+$files = @("aceedit.htm","animation.js","cmedit.htm","config.htm","index.htm","info.htm","littlefsb.htm","pumpmode.htm","reboot.htm","update.htm","upload.htm","wifiOiler.css")
+$count = 0
 
-Get-ChildItem $source | 
-Foreach-Object {
-    $targetFile = $target + "\" + $_.Name + ".gz"
-    $sourceFile = $source + "\" + $_.Name
-    $copyTarget = $target + "\" + $_.Name
-    if([System.IO.File]::Exists($targetFile)) {
-        # Datum vergleichen
-        if ((Get-Item $sourceFile).LastWriteTime -gt (Get-Item $targetFile).LastWriteTime) {
-            "muss neu geshrinkt werden..." + $copyTarget
-            Copy-Item $sourceFile -Destination $target
-            & .\_gzip.exe -f $copyTarget
-            (Get-Item $targetFile).LastWriteTime = (Get-Date)
-        }
+foreach ($file in $files) {
+    $sourceFile = $source + "\" + $file
+    $targetFile = $target + "\" + $file + ".gz"
+    if ([System.IO.File]::Exists($sourceFile) -and (![System.IO.File]::Exists($targetFile) -or (Get-Item $sourceFile).LastWriteTime -gt (Get-Item $targetFile).LastWriteTime)) {
+        "need update: " + $targetFile
+        Copy-Item $sourceFile -Destination $target
+        & .\_gzip.exe -f ($target + "\" + $file)
+        (Get-Item $targetFile).LastWriteTime = (Get-Date)
+        $count++
     }
+}
+if ($count -eq 0) {
+    "All files up to date - nothing to do......"
+} else {
+    "{0} file(s) updated - done!" -f $count
+}
+if (!$psISE) {
+    Write-Host "Press any key to continue..."
+    [void][System.Console]::ReadKey($true)
 }
