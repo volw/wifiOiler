09b9e64bf4508ded544f6e658ef1f334a627dc2f
volw <volw@gmx.de>
2023-06-29 14:49:51 +0200

    version update

--- Arduino/wifiOiler/wifiOiler.h
+++ Arduino/wifiOiler/wifiOiler.h
@@ -22,7 +22,7 @@
 // _NO_PUMP_: nur während Entwicklung sinnvoll: Pumpe wird dauerhaft deaktiviert, damit's nicht ständig piept
 // _CAPTIVE_PORTAL_: Access Point wird als Captive Portal erstellt (wie Anmeldeseite eines Hotspots z.B. in Hotels)
 // _NO_OLD_FILES_: wenn definiert, wird beim Update keine vorh. Datei mit Endung .old gesichert
-#define VERSION "4.2.030"
+#define VERSION "4.2.031"
 #define _DISPLAY_AVAILABLE_
 //#define _NO_PUMP_
 #define _CAPTIVE_PORTAL_
