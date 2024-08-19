0afa00fc00e4bea9fe350cde7654c5c9172aee38
volw <volw@gmx.de>
2024-08-19 21:08:01 +0200

    update copyright notice

--- Arduino/wifiOiler/wifiOiler.h
+++ Arduino/wifiOiler/wifiOiler.h
@@ -1,6 +1,6 @@
 /************************************************************************
  * wifiOiler, an automatic distance depending motorbike chain oiler
- * Copyright (C) 2019-2023, volw
+ * Copyright (C) 2019-2024, volw
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
